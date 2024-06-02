3cd20329b89b24a831c5fce0df172ccab849c4f7
Eric Ung <ungericw@tutanota.com>
2024-06-02 09:38:42 -0500

    Add project files.

---
+++ CMakeProject1/CMakeProject1.c
@@ -0,0 +1,58 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	int* q = nums;
+	for (int i = 0; i < numsSize; i++)
+	{
+		int* r = q+1;
+
+		for (int j = i+1; j < numsSize; j++)
+		{
+			if (*q == target - *r)
+			{
+				int* result = malloc(sizeof(int) * 2);
+				result[0] = i;
+				result[1] = j;
+				*returnSize = 2;
+				return result;
+			}
+			r++;
+		}
+
+		q++;
+	}
+
+	*returnSize = 0;
+	return NULL;
+} 
+
+int main() {
+	const int numsLength = 3; // const never changes
+	int nums[3] = {3,2,3};
+	int target;
+	int returnSize;
+
+	target = 6;
+
+	// 
+	// &returnSize is the address of the variable, returnSize, in the main procedure
+	int* result = twoSum(nums, numsLength, target, &returnSize);
+
+	if (result != NULL)
+	{
+		printf("%d,%d",result[0], result[1]);
+	}
+	else
+	{
+		printf("NULL");
+	}
+
+	return 0;
+}
